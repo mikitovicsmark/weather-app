4d905f20f3d7222d97514920d3ebf72caf1c7093
Mark Mikitovics <mikitovicsm@gmail.com>
2018-03-06 15:01:00 +0100

    feat(slider): added animation

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,14 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { WeatherCheckerService } from '../weather-checker.service';
 
 @Component({
   selector: 'wa-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate(500, style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class SliderComponent {
   @Input() filterData: Object;
-  constructor(private checkerService: WeatherCheckerService) { }
+  constructor(private checkerService: WeatherCheckerService) {}
 
   valueChange(label, value) {
     this.checkerService.setFilterValue(label, value);
@@ -17,5 +29,4 @@ export class SliderComponent {
   toleranceChange(label, tolerance) {
     this.checkerService.setFilterTolerance(label, tolerance);
   }
-
 }
